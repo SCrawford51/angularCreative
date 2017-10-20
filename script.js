97e4e64a0fd304005934b65981e482cdd19cf20a
Sean Crawford <Sheath51@gmail.com>
2017-10-20 19:33:57 +0000

    final code

--- script.js
+++ script.js
@@ -17,20 +17,14 @@ angular.module('myApp', [])
                 url: googleUrl,
                 dataType: "json",
                 success: function (parsed_json) {
-                    console.log(parsed_json);
                     lat = parsed_json.results[0].geometry.location.lat;
                     long = parsed_json.results[0].geometry.location.lng;
                     coord = lat + "," + long + "," + miles;
-                    console.log(coord);
                     myurl = 'https://api.betterdoctor.com/2016-03-01/practices?location=' + coord + '&skip=0&limit=10&user_key=' + apikey;
-                    console.log(myurl);
                     $http.get(myurl)
                         .then(function (response) {
-                            var size = Object.keys(response.data.data).length;
-                            for(i=0;i<size;i++) {
-                                console.log(response.data.data[i].doctors.profile);
-                                $scope.doctors = response.data.data[i].doctors.profile;
-                            }
+                            console.log(response.data.data[0].doctors);
+                            $scope.doctors = response.data.data[0].doctors;
                         });
                 }
             });
