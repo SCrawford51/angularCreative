cb8282209cbc4c05d1c3ae95fc2e113ae52377aa
Ubuntu <joshuasmcclellan@gmail.com>
2017-10-20 03:12:15 +0000

    fixed bugs

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const googleKey = "AIzaSyAxGH5zZbUiYeX8IalIM8Fqmk0J1Ptodpc";
-const miles = 100;
+const miles = 25;
 var lat;
 var long;
 
@@ -7,25 +7,29 @@ var long;
 angular.module('myApp', [])
     .controller('myCtrl', function ($scope, $http) {
         apikey = '220dcd61170e58ade5c6016e96164b07'
+        console.log('new');
         $scope.request = function (user) {
             city = user.location
             googleUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=";
             googleUrl += city;
             googleUrl += "&key=" + googleKey;
             $.ajax({
-                url: myurl,
+                url: googleUrl,
                 dataType: "json",
                 success: function (parsed_json) {
                     console.log(parsed_json);
                     lat = parsed_json.results[0].geometry.location.lat;
                     long = parsed_json.results[0].geometry.location.lng;
+                    coord = lat + "," + long + "," + miles;
+                    console.log(coord);
+                    myurl = 'https://api.betterdoctor.com/2016-03-01/practices?location=' + coord + '&skip=0&limit=10&user_key=' + apikey;
+                    console.log(myurl);
+                    $http.get(myurl)
+                        .then(function (response) {
+                            $scope.myWelcome = response.data;
+                        });
                 }
             });
-            location = lat + "," + long + "," + miles;
-            url = 'https://api.betterdoctor.com/2016-03-01/practices?location=' + location + '&skip=0&limit=10&user_key=' + apikey;
-            $http.get(url)
-                .then(function (response) {
-                    $scope.myWelcome = response.data;
-                });
+
         };
     });
